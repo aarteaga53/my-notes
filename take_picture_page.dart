0fa7ef5d6722378a35a076997bac06da2f7c0f4a
aarteaga53 <81718874+aarteaga53@users.noreply.github.com>
2021-09-21 09:42:47 -0700

    Added file that will take pictures
    This page will take a picture to upload to your notes

---
+++ take_picture_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TakePicturePage extends StatefulWidget {
+  const TakePicturePage({Key? key}) : super(key: key);
+
+  @override
+  _TakePicturePageState createState() => _TakePicturePageState();
+}
+
+class _TakePicturePageState extends State<TakePicturePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
