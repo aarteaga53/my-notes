c630cdc2b4f73183342d2525b9cc6a57c30a7812
aarteaga53 <81718874+aarteaga53@users.noreply.github.com>
2021-09-25 13:06:13 -0700

    Displays the image

--- note_page.dart
+++ note_page.dart
@@ -1,38 +1,96 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NotePage extends StatefulWidget {
   //const NotePage({Key? key}) : super(key: key);
 
   String title;
 
-  NotePage(this.title);
+  NotePage(this.title, {Key? key}) : super(key: key);
 
   @override
   _NotePageState createState() => _NotePageState();
 }
 
 class _NotePageState extends State<NotePage> {
+
+  String notePath = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadNotePath().then((path) {
+      notePath = path;
+      setState(() {
+
+      });
+    });
+  }
+
+  Future<String> loadNotePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    return '$path/' + widget.title + '.jpeg';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(widget.title)
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+
+              },
+              icon: const Icon(Icons.edit),
+
+    )
+        ],
+      ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 10,
-                  child: Icon(Icons.arrow_back)
+          Expanded(
+            flex: 90,
+            child: SizedBox(
+
+              child: Image(
+                  image: FileImage(File(notePath)),
+                fit: BoxFit.cover,
               ),
-              Expanded(
-                flex: 80,
-                  child: Center(child: Text(widget.title))
-              ),
-              Expanded(
-                flex: 10,
-                child: Icon(Icons.edit),
-              ),
-            ],
+            ),
           ),
+          Expanded(
+            flex: 10,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+
+                  },
+                  icon: const Icon(Icons.camera_alt),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+
+                  },
+                  icon: const Icon(Icons.reorder),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+
+                  },
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
