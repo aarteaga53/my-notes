d70395667ede2fa948f3dac590c704aafb8536d6
aarteaga53 <81718874+aarteaga53@users.noreply.github.com>
2021-09-28 21:54:11 -0700

    Creates PDF and changed floating action bar
    Takes a picture and turns it into a pdf, also can now pick an image from gallery to turn into a note, floating action bar now has three options

--- main.dart
+++ main.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mynotes/take_picture_page.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'note_page.dart';
 
 void main() {
@@ -18,8 +23,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Notes',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primaryColor: Colors.blueGrey,
       ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: const MyHomePage(title: 'My Notes'),
     );
   }
@@ -39,16 +46,50 @@ class _MyHomePageState extends State<MyHomePage> {
   int noteCounter = 1;
   List noteList = [];
 
-  void incrementNoteCounter() {
+  @override
+  void initState() {
+    super.initState();
     setState(() {
-      noteCounter++;
+      loadNoteCounter();
     });
   }
 
-  void decrementNoteCounter() {
+  void loadNoteCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    noteCounter = prefs.getInt('noteCounter') ?? 1;
+  }
+
+  void incrementNoteCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    noteCounter++;
+    prefs.setInt('noteCounter', noteCounter);
     setState(() {
-      noteCounter--;
+
     });
+  }
+
+  Future<String> getFilepath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    return '$path/Note' + noteCounter.toString();
+  }
+
+  void createNote(String filepath) {
+    var note = {
+      'image' : filepath,
+      'title' : 'Note' + noteCounter.toString(),
+    };
+
+    noteList.add(note);
+  }
+
+  void createPDF(var pdf, File picture) {
+    final image = pw.MemoryImage(picture.readAsBytesSync());
+    pdf.addPage(pw.Page(
+        pageFormat: PdfPageFormat.undefined,
+        build: (pw.Context context) {
+          return pw.Center(child: pw.Image(image));
+        }));
   }
 
   @override
@@ -74,18 +115,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     MaterialPageRoute(builder: (context) => NotePage(noteList[index])),
                   );
                 },
+                onLongPress: () {
+
+                },
                 title: Column(
                   children: [
-                    Container(
+                    SizedBox(
                       width: 100,
                       height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: FileImage(File(noteList[index]['image'])),
-                        ),
-                      ),
+                      child: IgnorePointer(child: SfPdfViewer.file(File(noteList[index]['image']))),
+
                     ),
                   ],
                 ),
@@ -95,18 +134,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text(noteList[index]['title']),
-                    // IconButton(
-                    //   onPressed: () async {
-                    //     final directory = Directory(noteList[index]['path']);
-                    //     await directory.delete(recursive: true);
-                    //     noteList.removeAt(index);
-                    //     setState(() {
-                    //
-                    //     });
-                    //     //decrementNoteCounter();
-                    //   },
-                    //   icon: const Icon(Icons.delete),
-                    // ),
                   ]
               ),
             ],
@@ -114,36 +141,62 @@ class _MyHomePageState extends State<MyHomePage> {
         },
 
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-           final cameras = await availableCameras();
-           final firstCamera = cameras.first;
+      floatingActionButton: SpeedDial(
+        icon: Icons.create,
+        children: [
+          SpeedDialChild(
+            onTap: () async {
+              final cameras = await availableCameras();
+              final firstCamera = cameras.first;
 
-           final result = await Navigator.push(
-               context,
-               MaterialPageRoute(builder: (context) => TakePictureScreen(camera: firstCamera))
-           );
-           final picture = File(result);
+              final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TakePictureScreen(camera: firstCamera))
+              );
+              
+              final picture = File(result);
+              var pdf = pw.Document();
+              createPDF(pdf, picture);
 
-           final directory = await getApplicationDocumentsDirectory();
-           final path = directory.path;
-           var newDirectory = '$path/note' + noteCounter.toString();
-           Directory(newDirectory).create(recursive: true);
-           var filename = newDirectory + '/Note1.jpeg';
-           final file = File(filename);
-           file.writeAsBytesSync(picture.readAsBytesSync());
+              String filepath = await getFilepath();
+              final file = File(filepath);
+              file.writeAsBytes(await pdf.save());
 
-           var note = {
-             'image' : filename,
-             'title' : 'Note' + noteCounter.toString(),
-             'path' : newDirectory,
-           };
+              createNote(filepath);
+              incrementNoteCounter();
+            },
+            label: 'Take Picture',
+            child: const Icon(Icons.add_a_photo),
+          ),
+          SpeedDialChild(
+            onTap: () async {
+              PickedFile? pickedFile = (await ImagePicker().pickImage(
+                source: ImageSource.gallery,
+              )) as PickedFile?;
 
-           noteList.add(note);
-           incrementNoteCounter();
-         },
-         child: const Icon(Icons.camera_alt),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              final imageFile = File(pickedFile!.path);
+              var pdf = pw.Document();
+              createPDF(pdf, imageFile);
+
+              String filepath = await getFilepath();
+              final file = File(filepath);
+              file.writeAsBytes(await pdf.save());
+
+              createNote(filepath);
+              incrementNoteCounter();
+            },
+            label: 'Add Image',
+            child: const Icon(Icons.add_photo_alternate),
+          ),
+          SpeedDialChild(
+            onTap: () {
+
+            },
+            label: 'Create Folder',
+            child: const Icon(Icons.create_new_folder),
+          )
+        ],
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
